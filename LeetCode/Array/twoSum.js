9fe8b1977f1c44db408257355c13de0731643a36
Sakshi Kataria <Katariasakshi2604@gmail.com>
2025-12-24 23:38:31 +0530

    initial commit

---
+++ LeetCode/Array/twoSum.js
@@ -0,0 +1,20 @@
+// Sum of two arrays to target value
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+const twoSum = (nums, target) => {
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = 1; j <= nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
